423222b3a76a7e1baf5ba0e4eb6c433525bd1b21
Chandra <Chandra@DESKTOP-ONCETEG>
2019-02-04 14:49:45 +0530

    Method Overriding

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,11 +8,63 @@ namespace Practice2
 
 
 {
-    class Program
+    public class Employee
+
     {
-        static void Main()
+        public string FirstName = "ABCD";
+        public string LastName = "EFGH";
+        public virtual void PrintFullName()
         {
-            Console.WriteLine("Hello");
+            Console.WriteLine(FirstName + " " + LastName);
+        }
+    }
+    public class FullTimeEmployee : Employee
+    {
+        public override void PrintFullName()
+        {
+            Console.WriteLine(FirstName + " " + LastName+"-FullTime");
+        }
+    }
+    public class PartTimeEmployee : Employee
+    {
+        public override void PrintFullName()
+        {
+            Console.WriteLine(FirstName + " " + LastName + "-PartTime");
+        }
+    }
+    public class TemporaryEmployee : Employee
+    {
+        public override void PrintFullName()
+        {
+            Console.WriteLine(FirstName + " " + LastName + "-Temporary");
+        }
+
+    }
+    class program
+    {
+        public static void Main()
+        {
+            Employee[] Employees = new Employee[4];
+            Employees[0] = new Employee();
+            Employees[1] = new FullTimeEmployee();
+            Employees[2] = new PartTimeEmployee();
+            Employees[3] = new TemporaryEmployee();
+
+
+            foreach (Employee e in Employees)
+            {
+                e.PrintFullName();
+            }
+
         }
     }
 }
+
+            
+
+       /* public static void Main()
+        {
+            Console.WriteLine("Hello");
+        }
+    }*/
+
